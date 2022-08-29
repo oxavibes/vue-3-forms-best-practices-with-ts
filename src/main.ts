6970b205f08db9e2edc961e3ecf89d8eae2a3853
oxavibes <bozzistef@gmail.com>
2022-08-29 18:07:46 -0300

    style: add some layout style

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import "./assets/css/base.css";
 import "./assets/css/app.css";
 
 const app = createApp(App);
